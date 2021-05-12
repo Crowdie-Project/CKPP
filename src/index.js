d46c4131a4139e3c8b8e4c256ef1a60ce1e6238c
madprogramer <themadprogramer@gmail.com>
2021-05-12 18:47:03 +0300

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
-//export some stuff
 
-//export { default as RECHandler } from './RECHandler.js';
-
-//export default ...
-
-import RECHandler from './RECHandler.js';
-
-//var tmp = new RECHandler();
-//RECHandler.test();
+export { default as RECHandler } from './RECHandler.js';
